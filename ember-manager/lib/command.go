4771a48a1f464a17713bdca961331554bfac06e6
monocle <hchw29@gmail.com>
2014-07-12 17:05:41 -0700

    create Command type

--- ember-manager/lib/command.go
+++ ember-manager/lib/command.go
@@ -4,45 +4,66 @@ import (
 	"io/ioutil"
 	"log"
 	"os/exec"
+	"strings"
 	"time"
 )
 
-func CommandFn(commandName string, args ...string) func(string, []byte, chan File) {
-	return func(path string, input []byte, res chan File) {
-		start := time.Now()
-		cmd := exec.Command(commandName, append(args, path, string(input))...)
+type Command struct {
+	Name   string
+	Args   []string
+	Source []byte
+	OutCs  []chan *File
+}
 
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
+func (c *Command) Run(f *File) {
+	start := time.Now()
 
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		}
-
-		content, err := ioutil.ReadAll(stdout)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(content) < 1 {
-			msg, err := ioutil.ReadAll(stderr)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Println(Color("[processor error]", "red"), path, string(msg))
-
-		} else {
-			log.Printf("%s %.2fs for %s\n", Color("[processing]", "magenta"), time.Since(start).Seconds(), path)
-			res <- File{path, content, nil}
-		}
+	if c.Name == "" {
+		c.OutCs[0] <- &File{}
+		return
 	}
 
+	args := append(c.Args, string(c.Source), f.Path, string(f.Content))
+	cmd := exec.Command(c.Name, args...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(content) < 1 {
+		msg, err := ioutil.ReadAll(stderr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(Color("[processor error]", "red"), f.Path, string(msg))
+
+	} else {
+		log.Printf("%s %.2fs for %s\n", Color("[processing]", "magenta"), time.Since(start).Seconds(), f.Path)
+
+		// TODO - do mo' better
+		split := strings.Split(string(content), "OUTPUT_PATH=")
+		if len(split) > 1 {
+			f.Path = split[1]
+		}
+
+		f.Content = []byte(split[0])
+		ch := c.OutCs[0]
+		ch <- f
+	}
 }
