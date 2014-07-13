b7d4aba3cdada6cdb653b8c2346938d986b18237
monocle <hchw29@gmail.com>
2014-07-12 17:07:28 -0700

    Config: create GetChanFor, init calls Processor#Start

--- ember-manager/lib/config.go
+++ ember-manager/lib/config.go
@@ -6,20 +6,26 @@ import (
 	"os"
 )
 
-type Processor struct {
-	Name     string
-	Dir      string
-	FileType string
+type config struct {
+	Processors []*Processor
+	Vendors    []string
 }
 
-type config struct {
-	Processors []Processor
-	Vendors    []string
+func (c *config) GetChanFor(name string) (ch chan *File) {
+	for _, p2 := range c.Processors {
+		if name == p2.Name {
+			ch = p2.InC
+			break
+		}
+	}
+	return
 }
 
 var Config config
 
 func init() {
+	MainChan = make(chan *File)
+
 	file, err := os.Open("ember-manager-config.json")
 	if err != nil {
 		log.Fatal("ember-manager-config.json file needed")
@@ -29,5 +35,9 @@ func init() {
 	Config = config{}
 	if err = json.NewDecoder(file).Decode(&Config); err != nil {
 		log.Fatal("error reading ember-manager-config.json ", err)
+	}
+
+	for _, p := range Config.Processors {
+		p.Start()
 	}
 }
